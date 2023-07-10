4fa0c5788253cfbcd2110511674a13a4272b9058
jhs512 <jangka512@gmail.com>
2023-07-10 11:24:08 +0900

    세팅

---
+++ src/test/java/com/example/tip/TipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
